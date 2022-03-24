7e5a3503fb797fb7640c26ff7cd2ab6f7311c864
scaneer <slcaneer42@students.tntech.edu>
2022-03-24 13:51:51 -0500

    rebase 1

--- modules/ecs/cluster/env-vars-for-ecs.tf
+++ modules/ecs/cluster/env-vars-for-ecs.tf
@@ -73,55 +73,6 @@ EOF
         }
 EOF
 
-  ecs_daw_shared_ssm_secrets = <<EOF
-        {
-          "name": "CLUSTER_NAME",
-          "valueFrom": "${aws_ssm_parameter.cluster_name.arn}"
-        },
-        {
-          "name": "AWS_REGION",
-          "valueFrom": "${aws_ssm_parameter.aws_region.arn}"
-        },
-        {
-          "name": "SES_PORT",
-          "valueFrom": "${aws_ssm_parameter.ses_port.arn}"
-        },
-        {
-          "name": "SES_SERVER",
-          "valueFrom": "${aws_ssm_parameter.ses_server.arn}"
-        },
-        {
-          "name": "SES_FROM_EMAIL",
-          "valueFrom": "${aws_ssm_parameter.ses_from_email.arn}"
-        },
-        {
-          "name": "SMTP_USERNAME",
-          "valueFrom": "${aws_ssm_parameter.smtp_username.arn}"
-        },
-        {
-          "name": "SMTP_PASSWORD",
-          "valueFrom": "${aws_ssm_parameter.smtp_password.arn}"
-        },
-        {
-            "name": "SECRET_KEY_BASE",
-            "valueFrom": "${aws_ssm_parameter.secret_key_base.arn}"
-        }
-EOF
-
-  ecs_dac_shared_ssm_secrets = <<EOF
-        {
-            "name": "HOST",
-            "valueFrom": "${aws_ssm_parameter.host.arn}"
-        }
-EOF
-
-  ecs_da_shared_ssm_secrets = <<EOF
-        {
-            "name": "PORT",
-            "valueFrom": "${aws_ssm_parameter.port.arn}"
-        }
-EOF
-
   ecs_shared_env_vars = <<EOF
         { "name" : "MIX_ENV", "value" : "${var.profile}" },
         { "name" : "MAX_ATTEMPTS_BEFORE_MANUAL_VERIFICATION", "value" : "${var.max_attempts_before_manual_verification}" },
