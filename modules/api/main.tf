d0e0c5d2e0a7e46fdda3bbe84795e481542741b0
Sam Caneer <slcaneer42@students.tntech.edu>
2022-03-24 09:29:06 -0500

    rebase 4

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -401,11 +401,9 @@ resource "aws_ecs_task_definition" "api_task_definition" {
        ],
        "secrets": [
           ${local.ecs_shared_ssm_secrets},
-          ${local.ecs_daw_shared_ssm_secrets},
-          ${local.ecs_da_shared_ssm_secrets}
           {
-            "name": "CA_HOST",
-            "valueFrom": "${aws_ssm_parameter.ca_host.arn}"
+            "name": "CLUSTER_NAME",
+            "valueFrom": "${aws_ssm_parameter.cluster_name.arn}"
           }
         ],
        "volumesFrom": [],
