cd854915ea9e433a3fde1e578a8bc8467bb74e8c
Sam Caneer <slcaneer42@students.tntech.edu>
2022-03-24 08:34:37 -0500

    rebase 3

--- modules/ecs/cluster/env-vars-for-ecs.tf
+++ modules/ecs/cluster/env-vars-for-ecs.tf
@@ -10,45 +10,6 @@ locals {
             "valueFrom": "${aws_ssm_parameter.erl_cookie.arn}"
         },
         {
-            "name": "APP_NAME",
-            "valueFrom": "${aws_ssm_parameter.app_name.arn}"
-        },
-        {
-            "name": "S3_BUCKET_NAME",
-            "valueFrom": "${aws_ssm_parameter.s3_bucket_name.arn}"
-        }
-EOF
-
-  ecs_daw_shared_ssm_secrets = <<EOF
-        {
-          "name": "CLUSTER_NAME",
-          "valueFrom": "${aws_ssm_parameter.cluster_name.arn}"
-        },
-        {
-          "name": "AWS_REGION",
-          "valueFrom": "${aws_ssm_parameter.aws_region.arn}"
-        },
-        {
-          "name": "SES_PORT",
-          "valueFrom": "${aws_ssm_parameter.ses_port.arn}"
-        },
-        {
-          "name": "SES_SERVER",
-          "valueFrom": "${aws_ssm_parameter.ses_server.arn}"
-        },
-        {
-          "name": "SES_FROM_EMAIL",
-          "valueFrom": "${aws_ssm_parameter.ses_from_email.arn}"
-        },
-        {
-          "name": "SMTP_USERNAME",
-          "valueFrom": "${aws_ssm_parameter.smtp_username.arn}"
-        },
-        {
-          "name": "SMTP_PASSWORD",
-          "valueFrom": "${aws_ssm_parameter.smtp_password.arn}"
-        },
-        {
             "name": "SECRET_KEY_BASE",
             "valueFrom": "${aws_ssm_parameter.secret_key_base.arn}"
         },
