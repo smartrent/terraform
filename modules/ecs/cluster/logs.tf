dfdd8d32aa3b9f630bc160410c05e3431a502852
scaneer <slcaneer42@students.tntech.edu>
2022-03-25 15:45:02 -0500

    correct placement of log module

--- modules/ecs/cluster/logs.tf
+++
@@ -1,9 +0,0 @@
-module "log_config" {
-  source              = "../../../modules/firelens_log_config"
-  app_name            = var.app_name
-  datadog_key_ssm_arn = aws_ssm_parameter.datadog_key.arn
-  environment_name    = var.profile
-  task_name           = local.web_task_name
-  datadog_image       = var.datadog_image
-  aws_region          = var.aws_region
-}
