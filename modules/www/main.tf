b0be6889032521eb93d172cd3bf06fdda5d285d7
scaneer <slcaneer42@students.tntech.edu>
2022-03-25 16:20:27 -0500

    test on www

--- modules/www/main.tf
+++ modules/www/main.tf
@@ -482,3 +482,22 @@ resource "aws_ecs_service" "www_ecs_service" {
     aws_lb_listener.www_lb_listener
   ]
 }
+
+resource "aws_ssm_parameter" "datadog_key" {
+  name   = "${local.ssm_prefix}DATADOG_KEY"
+  type   = "SecureString"
+  value  = "ChangeMeInTheWebConsole"
+  key_id = aws_kms_key.for_ssm_params.key_id
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_kms_key" "for_ssm_params" {
+  description = "KMS key for ${var.app_name} ${var.environment_name} Secrets"
+
+  tags = var.tags
+}
