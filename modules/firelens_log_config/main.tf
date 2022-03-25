979238db1ef8da6f01215dac55fbdaa24d5b12d6
scaneer <slcaneer42@students.tntech.edu>
2022-03-25 15:45:02 -0500

    correct placement of log module

--- modules/firelens_log_config/main.tf
+++ modules/firelens_log_config/main.tf
@@ -1,5 +1,15 @@
 # This is just a module to provide a template for creating a log configuration for an ECS task definition.
 
+module "log_config" {
+  source              = "../../../modules/firelens_log_config"
+  app_name            = var.app_name
+  datadog_key_ssm_arn = aws_ssm_parameter.datadog_key.arn
+  environment_name    = var.profile
+  task_name           = local.web_task_name
+  datadog_image       = var.datadog_image
+  aws_region          = var.aws_region
+}
+
 locals {
   tld = var.aws_region == "eu-central-1" ? "eu" : "com"
 
