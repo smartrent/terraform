df1914b8b6d8e5b45514687b4ca58bb59de9f2ed
Melissa Greenbaum <melissa.greenbaum@smartrent.com>
2022-04-08 14:42:36 -0400

    update route53 for alb

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -5,6 +5,9 @@ variable "api_dns_record_name" {}
 variable "device_dns_record_name" {}
 variable "www_dns_record_name" {}
 variable "dns_zone" {}
+variable "is_api_alb" {
+  default = false
+}
 
 variable "tags" {
   description = "A mapping of tags to assign to the resources"
