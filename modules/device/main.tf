1bdc3339096070c924ce8613b435cbd3ad48c4b3
scaneer <slcaneer42@students.tntech.edu>
2022-04-09 18:05:11 -0500

    jk let's do it this way

--- modules/device/main.tf
+++ modules/device/main.tf
@@ -3,6 +3,32 @@
 locals {
   device_app_name = "nerves_hub_device"
   ssm_prefix = "nerves_hub_device"
+
+  ecs_shared_env_vars = <<EOF
+    { "name" : "ENVIRONMENT", "value" : "${var.environment_name}" },
+    { "name" : "APP_NAME", "value" : "${local.app_name}" }
+EOF
+
+  fire_lens_container = <<EOF
+  {
+    "essential": true,
+    "image": "906394416424.dkr.ecr.${var.region}.amazonaws.com/aws-for-fluent-bit:stable",
+    "name": "log_router",
+    "cpu": 0,
+    "user": "0",
+    "environment": [],
+    "volumesFrom": [],
+    "portMappings": [],
+    "mountPoints": [],
+    "firelensConfiguration": {
+      "type": "fluentbit",
+      "options": {
+        "enable-ecs-log-metadata": "true"
+      }
+    },
+    "memoryReservation": 50
+  }
+EOF
 }
 
 resource "random_integer" "target_group_id" {
