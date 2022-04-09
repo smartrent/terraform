79b943a859ee747d8e1d653affaf56e2a45967e1
scaneer <slcaneer42@students.tntech.edu>
2022-04-09 18:31:53 -0500

    remove environment and replace with environment_name

--- modules/ecs/cluster/variables.tf
+++ modules/ecs/cluster/variables.tf
@@ -6,10 +6,6 @@ variable "aws_vpc_id" {
   description = "VPC id"
 }
 
-variable "environment" {
-  description = "Deploy environment"
-}
-
 variable "aws_private_subnet_ids" {
   description = "Subnet ids"
 }
