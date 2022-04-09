14007aa5f895f5e99dcc9d51558785e53137c4e9
scaneer <slcaneer42@students.tntech.edu>
2022-04-09 18:09:33 -0500

    api variable corrections in container

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -427,10 +427,10 @@ resource "aws_ecs_task_definition" "api_task_definition" {
             "Name": "datadog",
             "compress": "gzip",
             "Host": "http-intake.logs.datadoghq.com",
-            "dd_service": "${var.app_name}",
+            "dd_service": "${local.app_name}",
             "dd_source": "elixir",
             "dd_message_key": "log",
-            "dd_tags": "env:${var.profile},application:${var.app_name}-${var.profile},version:${var.docker_image}",
+            "dd_tags": "env:${var.environment_name},application:${local.app_name}-${var.environment_name},version:${var.docker_image}",
             "TLS": "on",
             "provider": "ecs"
           },
