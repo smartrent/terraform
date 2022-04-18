77a150dcbd2b3a70b3cf44e0393871c5a799fa1a
scaneer <slcaneer42@students.tntech.edu>
2022-04-18 11:04:40 -0500

    bracket fix

--- modules/device/main.tf
+++ modules/device/main.tf
@@ -513,7 +513,6 @@ resource "aws_ecs_task_definition" "device_task_definition" {
               "valueFrom": "${aws_ssm_parameter.datadog_key.arn}"
             }
           ]
-         }
        }
      },
      ${local.datadog_ecs_agent_task_def}
