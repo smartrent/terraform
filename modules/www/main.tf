5389a7ddcf514d1918f861c93b2709b62d7878b4
scaneer <slcaneer42@students.tntech.edu>
2022-04-20 15:52:41 -0500

    try try again

--- modules/www/main.tf
+++ modules/www/main.tf
@@ -399,7 +399,10 @@ resource "aws_ecs_task_definition" "www_task_definition" {
   container_definitions = <<DEFINITION
    [
      ${module.firelens_log_config.fire_lens_container},
-     ${module.firelens_log_config.datadog_container},
+     {
+      ${module.firelens_log_config.datadog_container},
+      ${module.firelens_log_config.log_configuration}
+     },
      {
        "portMappings": [
          {
