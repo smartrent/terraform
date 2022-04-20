8ab8a8cb625560fdf2cae5333c53c6d5647ce2fb
scaneer <slcaneer42@students.tntech.edu>
2022-04-20 16:02:09 -0500

    revert

--- modules/www/main.tf
+++ modules/www/main.tf
@@ -8,6 +8,139 @@ locals {
     { "name" : "ENVIRONMENT", "value" : "${var.environment_name}" },
     { "name" : "APP_NAME", "value" : "${local.app_name}" }
 EOF
+
+  tld = var.region == "eu-central-1" ? "eu" : "com"
+
+  fire_lens_container = <<EOF
+  {
+    "essential": true,
+    "image": "906394416424.dkr.ecr.${var.region}.amazonaws.com/aws-for-fluent-bit:stable",
+    "name": "log_router",
+    "cpu": 0,
+    "user": "0",
+    "environment": [],
+    "volumesFrom": [],
+    "portMappings": [],
+    "mountPoints": [],
+    "firelensConfiguration": {
+      "type": "fluentbit",
+      "options": {
+        "enable-ecs-log-metadata": "true"
+      }
+    },
+    "memoryReservation": 50
+  }
+EOF
+
+  datadog_ecs_agent_task_def = <<EOF
+  {
+    "name": "datadog-agent",
+    "image": "${var.datadog_image}",
+    "essential": false,
+    "memoryReservation": 256,
+    "cpu": 10,
+    "mountPoints": [],
+    "volumesFrom": [],
+    "portMappings": [
+      {
+        "containerPort": 8125,
+        "hostPort": 8125,
+        "protocol": "udp"
+      },
+      {
+        "containerPort": 8126,
+        "hostPort": 8126,
+        "protocol": "tcp"
+      }
+    ],
+    "environment": [
+      {
+        "name": "ECS_FARGATE",
+        "value": "true"
+      },
+      {
+        "name": "DD_LOG_LEVEL",
+        "value": "warn"
+      },
+      {
+        "name": "DD_APM_ENABLED",
+        "value": "true"
+      },
+      {
+        "name": "DD_APM_NON_LOCAL_TRAFFIC",
+        "value": "true"
+      },
+      {
+        "name": "DD_SYSTEM_PROBE_ENABLED",
+        "value": "true"
+      },
+      {
+        "name": "DD_PROCESS_AGENT_ENABLED",
+        "value": "true"
+      },
+      {
+        "name": "DD_HEALTH_PORT",
+        "value": "5555"
+      },
+      {
+        "name": "DD_APM_RECEIVER_PORT",
+        "value": "8126"
+      },
+      {
+        "name": "DD_DOGSTATSD_NON_LOCAL_TRAFFIC",
+        "value": "true"
+      },
+      {
+        "name": "DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL",
+        "value": "true"
+      },
+      {
+        "name": "DD_DOGSTATSD_PORT",
+        "value": "8125"
+      },
+      {
+        "name": "DD_DOCKER_LABELS_AS_TAGS",
+        "value": "${replace(jsonencode(var.tags), "\"", "\\\"")}"
+      }
+    ],
+    "secrets": [
+      {
+        "name": "DD_API_KEY",
+        "valueFrom": "${module.firelens_log_config.datadog_key_arn}"
+      }
+    ]
+  }
+EOF
+
+  log_configuration = <<EOF
+"mountPoints": [],
+"volumesFrom": [],
+"logConfiguration": {
+      "logDriver": "awsfirelens",
+      "options": {
+        "Name": "datadog",
+        "compress": "gzip",
+        "Host": "http-intake.logs.datadoghq.${local.tld}",
+        "dd_service": "${var.app_name}",
+        "dd_source": "${var.dd_source}",
+        "dd_message_key": "log",
+        "dd_tags": "env:${var.environment_name},application:${var.app_name}-${var.environment_name},version:${var.datadog_image},task:${var.task_name}",
+        "TLS": "on",
+        "provider": "ecs"
+      },
+      "secretOptions": [
+        {
+          "name": "apikey",
+          "valueFrom": "${module.firelens_log_config.datadog_key_arn}"
+        }
+      ]
+    },
+    "dockerLabels": {
+      "com.datadoghq.tags.env": "${var.environment_name}",
+      "com.datadoghq.tags.service": "${var.app_name}",
+      "com.datadoghq.tags.version": "${var.datadog_image}"
+    }
+EOF
 }
 
 resource "random_integer" "target_group_id" {
@@ -398,31 +531,30 @@ resource "aws_ecs_task_definition" "www_task_definition" {
 
   container_definitions = <<DEFINITION
    [
-     ${module.firelens_log_config.fire_lens_container},
+     ${local.fire_lens_container},
      {
-      ${module.firelens_log_config.datadog_container},
-      ${module.firelens_log_config.log_configuration}
+      ${local.datadog_ecs_agent_task_def},
+      ${local.log_configuration}
      },
      {
-       "portMappings": [
-         {
-           "hostPort": 80,
-           "protocol": "tcp",
-           "containerPort": 80
-         },
-         {
-           "hostPort": 4369,
-           "protocol": "tcp",
-           "containerPort": 4369
-         }
-       ],
-       "networkMode": "awsvpc",
-       "image": "${var.docker_image}",
-       "essential": true,
-       "privileged": false,
-       "name": "${local.app_name}",
-       "environment": "${local.ecs_shared_env_vars}"
-     }
+        "portMappings": [{
+            "hostPort": 80,
+            "protocol": "tcp",
+            "containerPort": 80
+          },
+          {
+            "hostPort": 4369,
+            "protocol": "tcp",
+            "containerPort": 4369
+          }
+        ],
+        "networkMode": "awsvpc",
+        "image": "${var.docker_image}",
+        "essential": true,
+        "privileged": false,
+        "name": "${local.app_name}",
+        "environment": "${local.ecs_shared_env_vars}"
+      }
    ]
 DEFINITION
 
