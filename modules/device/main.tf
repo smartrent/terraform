6db5d992e3ad5957062723a824b599ae845d6c3c
scaneer <slcaneer42@students.tntech.edu>
2022-04-21 08:32:12 -0500

    argh

--- modules/device/main.tf
+++ modules/device/main.tf
@@ -7,7 +7,7 @@ locals {
   ecs_shared_env_vars = <<EOF
     { "name" : "ENVIRONMENT", "value" : "${terraform.workspace}" },
     { "name" : "APP_NAME", "value" : "${local.device_app_name}" },
-    { "name" : "S3_BUCKET_NAME", "value" : "${aws_ssm_parameter.nerves_hub_device_ssm_s3_bucket_name}" }
+    { "name" : "S3_BUCKET_NAME", "value" : "${aws_ssm_parameter.nerves_hub_device_ssm_s3_bucket_name.name}" }
 EOF
 
   fire_lens_container = <<EOF
