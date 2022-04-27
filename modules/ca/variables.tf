e9933eedbf4d31559ccea8d36245de973afbed3b
scaneer <slcaneer42@students.tntech.edu>
2022-04-27 12:18:28 -0500

    TECH-1560: Add Datadog Agent to CA Module

--- modules/ca/variables.tf
+++ modules/ca/variables.tf
@@ -36,3 +36,12 @@ variable "s3_prefix" {
   description = "S3 bucket name prefix"
   default     = ""
 }
+
+variable "environment_name" {
+  type = string
+}
+
+variable "datadog_image" {
+  description = "Datadog container image"
+  type = string
+}
