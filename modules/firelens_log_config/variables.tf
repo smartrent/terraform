20d695b28798d65f87661395bd08d2e277e8ec17
scaneer <slcaneer42@students.tntech.edu>
2022-04-27 12:30:25 -0500

    TECH-1560: Add Firelens Logging to Nerves Hub Terraform

---
+++ modules/firelens_log_config/variables.tf
@@ -0,0 +1,37 @@
+variable "environment_name" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "task_name" {
+  type = string
+}
+
+variable "datadog_image" {
+  type = string
+}
+
+variable "dd_source" {
+  type    = string
+  default = "elixir"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS Region"
+}
+
+variable "ssm_prefix" {
+  type = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resources"
+  type        = map(string)
+  default = {
+    terraform = true
+  }
+}
