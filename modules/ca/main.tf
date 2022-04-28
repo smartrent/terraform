d9d653244daee857ef8b35170b0fef62e36bdb28
scaneer <slcaneer42@students.tntech.edu>
2022-04-28 18:53:12 -0500

    remove duplicates

--- modules/ca/main.tf
+++ modules/ca/main.tf
@@ -356,29 +356,7 @@ resource "aws_ecs_task_definition" "ca_task_definition" {
        "name": "nerves_hub_ca",
        "environment": [
          ${local.ecs_shared_env_vars}
-       ],
-       "volumesFrom": [],
-       "mountPoints": [],
-       "logConfiguration": {
-         "logDriver": "awsfirelens",
-         "options": {
-            "Name": "datadog",
-            "compress": "gzip",
-            "Host": "http-intake.logs.datadoghq.com",
-            "dd_service": "${local.app_name}",
-            "dd_source": "elixir",
-            "dd_message_key": "log",
-            "dd_tags": "env:${var.environment_name},application:${local.app_name}-${var.environment_name},version:${var.docker_image}",
-            "TLS": "on",
-            "provider": "ecs"
-          },
-          "secretOptions": [
-            {
-              "name": "apikey",
-              "valueFrom": "${module.firelens_log_config.datadog_key_arn}"
-            }
-          ]
-       }
+       ]
      },
      ${module.firelens_log_config.datadog_container}
    ]
