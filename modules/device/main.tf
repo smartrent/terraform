da2a6aa6dca8ab16399f1e69a85ab8edc206e6ff
scaneer <slcaneer42@students.tntech.edu>
2022-05-13 10:42:02 -0500

    updates to datadog key arn

--- modules/device/main.tf
+++ modules/device/main.tf
@@ -414,7 +414,7 @@ module "firelens_log_config" {
   task_name           = local.device_app_name
   datadog_image       = var.datadog_image
   datadog_image_tag   = var.datadog_image_tag
-  datadog_key         = var.datadog_key
+  datadog_key_arn     = var.datadog_key.arn
   region              = var.region
   ssm_prefix          = local.ssm_prefix
   kms_key_id      = var.kms_key.arn
