fc4cc8d67f071defec05718fab1b18b9cbb09934
scaneer <slcaneer42@students.tntech.edu>
2022-05-23 15:17:35 -0500

    updates

--- modules/ca/variables.tf
+++ modules/ca/variables.tf
@@ -43,5 +43,15 @@ variable "environment_name" {
 
 variable "datadog_image" {
   description = "Datadog container image"
-  type = string
+  type        = string
+}
+
+variable "datadog_image_tag" {
+  description = "Datadog container image tag"
+  type        = string
+}
+
+variable "datadog_key_arn" {
+  description = "Datadog Key"
+  type        = string
 }
