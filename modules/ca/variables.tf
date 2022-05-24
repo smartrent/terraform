9527a6f08f9734b4d77e9f534427b45f8dcd1a6a
scaneer <slcaneer42@students.tntech.edu>
2022-05-24 10:41:26 -0500

    remove datadog image tag

--- modules/ca/variables.tf
+++ modules/ca/variables.tf
@@ -46,8 +46,8 @@ variable "datadog_image" {
   type        = string
 }
 
-variable "datadog_image_tag" {
-  description = "Datadog container image tag"
+variable "docker_image_tag" {
+  description = "Docker Image tag for CA Application"
   type        = string
 }
 
