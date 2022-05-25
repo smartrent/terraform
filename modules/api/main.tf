e45bd07bac4404cfd05fb0a37de8b4bf17aecb6f
scaneer <slcaneer42@students.tntech.edu>
2022-05-25 08:51:45 -0500

    remove datadog_key_arn from exec role

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -260,7 +260,6 @@ data "aws_iam_policy_document" "api_iam_policy" {
 
     resources = [
       "arn:aws:ssm:${var.region}:${var.account_id}:parameter/${local.app_name}/${terraform.workspace}*",
-      var.datadog_key_arn
     ]
   }
 
